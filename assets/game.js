6ecf8c305f85ba85db0cdf5a98c4e3659963a10a
Alapan Bhakat <alapanbhakat@gmail.com>
2019-04-23 15:20:05 -0700

    Revised

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,5 @@
 var yourScore = $("#yourScore");
-//var yourScore = getElementById(yellow, green, red, blue.onclick(math.sum));
+//var yourScore = (yellow, green, red, blue.onclick(math.sum));
 var scoreCurrent = 0;
 var Target = $("#Target");
 var scoreTarget = getRandomValue(10, 4);
@@ -7,59 +7,59 @@ Target.text(scoreTarget);
 
 function addToScore(val) {
   var numberToAdd = parseInt(val);
- scoreCurrent += val;
-  
+  scoreCurrent += numberToAdd;
+
   yourScore.text(scoreCurrent);
 }
 
 var yellow = $("#yellow");
 console.log(yellow.value);
 
-var green = document.getElementById("green");
-//console.log(green.value);
+var green = $("#green");
+console.log(green.value);
 
-var red = document.getElementById("red");
-//console.log(red.value);
+var red = $("#red");
+console.log(red.value);
 
-var blue = document.getElementById("blue");
-//console.log(blue.value);
+var blue = $("#blue");
+console.log(blue.value);
 
-yellow.addEventListener('click', function() {
+yellow.on('click', function () {
   //this refers to yellow
   addToScore(this.value);
   checkPlayerScore();
 });
 
-green.addEventListener('click', function() {
+green.on('click', function () {
   // this refers to green
   addToScore(this.value);
   checkPlayerScore();
 });
 
-red.addEventListener('click', function() {
+red.on('click', function () {
   // this refers to red
   addToScore(this.value);
   checkPlayerScore();
 });
 
-blue.addEventListener('click', function() {
+blue.on('click', function () {
   // this refers to blue
   addToScore(this.value);
   checkPlayerScore();
 });
 
-function getRandomValue (a, b){
+function getRandomValue(a, b) {
   return Math.floor((Math.random() * a) + b);
 }
 
 function checkPlayerScore() {
-  var playerInt = parseInt(yourScore.innerHTML);
-  var targetInt = parseInt(Target.innerHTML);
+  playerInt.text(yourScore);
+  Target.text(scoreTarget);
   if (playerInt === targetInt) {
-      alert('Win!');
+    alert('Win!');
   } else if (playerInt > targetInt) {
     alert('Lose!');
-  } 
+  }
 }
 
 console.log(getRandomValue(10, 4));
